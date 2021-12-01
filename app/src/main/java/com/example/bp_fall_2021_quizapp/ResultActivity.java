ec7e5635a53c6fddd0f4533ef4603b8cf9e88dee
riashah345 <riashah345@gmail.com>
2021-12-01 13:52:15 -0500

    Update ResultActivity.java

--- app/src/main/java/com/example/bp_fall_2021_quizapp/ResultActivity.java
+++ app/src/main/java/com/example/bp_fall_2021_quizapp/ResultActivity.java
@@ -2,6 +2,8 @@ package com.example.bp_fall_2021_quizapp;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -15,6 +17,24 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
 
         // initialize UI components
+        first = (TextView) findViewById(R.id.res1);
+        second = (TextView) findViewById(R.id.res2);
+        third = (TextView) findViewById(R.id.res3);
+        restartButton = (Button) findViewById((R.id.restartButton));
+
+        StringBuffer sb1 = new StringBuffer();
+        sb1.append("Correct answers: " + QuizQuestionActivity.correct + "\n");
+        StringBuffer sb2 = new StringBuffer();
+        sb2.append("Wrong Answers: " + QuizQuestionActivity.wrong + "\n");
+        StringBuffer sb3 = new StringBuffer();
+        sb3.append("Final Score: " + QuizQuestionActivity.correct + "\n");
+        first.setText(sb1);
+        second.setText(sb2);
+        third.setText(sb3);
+
+        QuestionsActivity.correct=0;
+        QuestionsActivity.wrong=0;
+
 
         // set username and score
     }

08f7078f1564562c2eaba04dc01cfb0f58f1f22a
riashah345 <riashah345@gmail.com>
2021-12-01 19:43:47 -0500

    update

--- app/src/main/java/com/example/bp_fall_2021_quizapp/QuizQuestionActivity.java
+++ app/src/main/java/com/example/bp_fall_2021_quizapp/QuizQuestionActivity.java
@@ -14,7 +14,6 @@ import java.util.List;
 public class QuizQuestionActivity extends AppCompatActivity {
 
     // UI components here
-
     private TextView questionTextView;
     private RadioGroup radioGroup;
     private ProgressBar progress;
@@ -38,6 +37,8 @@ public class QuizQuestionActivity extends AppCompatActivity {
         // create arraylist of questions
         questions = new ArrayList<>();
         // get username intent from main activity screen
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
 
         // initialize views using findViewByID
         questionTextView.findViewById(R.id.questionTextView);
@@ -57,10 +58,34 @@ public class QuizQuestionActivity extends AppCompatActivity {
         questionTotal = questions.size();
 
         // set progress bar
+        progress.setProgress(Integer.parseInt("Question: "+questionCounter+"/"+questionTotal));
 
         // use helper method to proceed to next question
         showNextQuestion();
+    }
 
+    /**
+     * Method that adds questions to our questions arraylist, using the Question Model constructor
+     */
+    private void addQuestions(){
+        // question 1
+        questions.add(new QuestionModel("What is the most stolen food item in the world?", "Bread", "Cheese", "Milk","Fruits", 2));
+        // question 2
+        questions.add(new QuestionModel("How many cups of coffee does Starbucks sell a day?", "87,000", "56,000", "124,000","202,000", 1));
+        // question 3
+        questions.add(new QuestionModel("What is the most expensive spice by weight in the world?", "Saffron", "Cardamom", "Vanilla bean","Fennel", 1));
+        // question 4
+        questions.add(new QuestionModel("What food never goes bad?", "Chocolate", "Potatoes", "Avocados", "Honey", 4));
+        // question 5
+        questions.add(new QuestionModel("What is not considered a fruit?", "Olives", "Cucumber", "Kale", "Avocado", 3));
+
+    }
+
+    /**
+     * Check the answer when user clicks submit and move on to next question
+     */
+    public void submitQuestion(View view){
+        // if no options have been selected, prompt user to select an answer
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -72,6 +97,8 @@ public class QuizQuestionActivity extends AppCompatActivity {
                         Toast.makeText(QuizQuestionActivity.this, "Please choose an answer", Toast.LENGTH_SHORT).show();
                     }
                 }
+
+        // use helper methods to check the answer and show the next question
                 else {
                     showNextQuestion();
                 }
@@ -80,37 +107,9 @@ public class QuizQuestionActivity extends AppCompatActivity {
     }
 
     /**
-     * Method that adds questions to our questions arraylist, using the Question Model constructor
-     */
-    private void addQuestions(){
-        // question 1
-        questions.add(new QuestionModel("Quanto que vale o número de Euler?", "C", "3.1415", "1.7189","2.7182", "5.985"));
-        // question 2
-        questions.add(new QuestionModel("Quem foi que disse a seguinte frase: \"Ame o seu próximo como a si mesmo\"?", "A", "Jesus", "Hitler","Mussolini", "Stalin"));
-        // question 3
-        questions.add(new QuestionModel("Quem é Friedrich Nietzsche?", "D", "Pedreiro", "Programador","Músico", "Filósofo"));
-        // question 4
-        questions.add(new QuestionModel("A música \"Billie Jean\" é cantada por quem?", "B", "Whindersson Nunes", "Michael Jackson", "Kanye West", "Billie Eilish"));
-        // question 5
-        questions.add(new QuestionModel("O que é um Ukulele?", "A", "Instrumento Musical", "Comida", "Empresa", "Time de Futebol"));
-
-    }
-
-    /**
-     * Check the answer when user clicks submit and move on to next question
-     */
-    public void submitQuestion(View view){
-        // if no options have been selected, prompt user to select an answer
-
-        // use helper methods to check the answer and show the next question
-
-    }
-
-    /**
      * Display next question. If finished, move onto results screen
      */
     private void showNextQuestion(){
-
         // clear previous button selections
         radioGroup.clearCheck();
         rb1.setTextColor(color);
@@ -118,6 +117,8 @@ public class QuizQuestionActivity extends AppCompatActivity {
         rb3.setTextColor(color);
         rb4.setTextColor(color);
 
+        // if you haven't gone through all the questions yet
+        // set the question & text to the next question
         if(questionCounter < questionTotal) {
             currQuestion = questions.get(questionCounter);
             questionTextView.setText(currQuestion.getQuestion());
@@ -126,21 +127,19 @@ public class QuizQuestionActivity extends AppCompatActivity {
             rb3.setText(currQuestion.getOpt3());
             rb4.setText(currQuestion.getOpt4());
 
+        // increase question number
             questionCounter++;
             nextButton.setText("Submit");
+        // set progress bar
             progress.setProgress(Integer.parseInt("Question: "+questionCounter+"/"+questionTotal));
             answeredQuestion = false;
 
+        // if finished with quiz, start Results activity
         } else {
+            Intent intent = new Intent(this, ResultActivity.class);
+            startActivity(intent);
             finish();
         }
-        // if you haven't gone through all the questions yet
-        // set the question & text to the next question
-        // increase question number
-        // set progress bar
-
-        // if finished with quiz, start Results activity
-
     }
 
     /**
@@ -149,7 +148,7 @@ public class QuizQuestionActivity extends AppCompatActivity {
     private void checkAnswer(){
         // see which answer they picked
         answeredQuestion = true;
-        RadioButton buttonSelected = findViewById(radioGroup.radioButtonCheckedID());
+        RadioButton buttonSelected = findViewById(radioGroup.getCheckedRadioButtonId());
         int answerNumber = radioGroup.indexOfChild(buttonSelected) + 1;
 
         // increase score if correct
@@ -178,5 +177,5 @@ public class QuizQuestionActivity extends AppCompatActivity {
         else {
             nextButton.setText("Finish");
         }
-    }//
+    }
 }
